9f2c767adc90ee467be0db03ba29f36967976328
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-28 02:35:11 +0300

    M

--- src/Commponents/Port.js
+++ src/Commponents/Port.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function Port() {
+  const arr = [
+    "assets/Portfolio/Hotel.png",
+    "assets/Portfolio/Store.png",
+    "assets/Portfolio/Marketing.png",
+    "assets/Portfolio/Food.png",
+    "assets/Portfolio/Coffee.png",
+    "assets/Portfolio/Blog.png",
+  ];
   return (
-<div className="port" id='Portfolio'>
-    <div className="container">
-        
+    <div className="port" id="Portfolio">
+      <div className="container">
+        {arr.map((src , index)=>{
+            return (
+                <div className="box"  key={index}>
+                    <img src={src} alt="" />
+                    <div className="absolute">
+                        <h2>Click me !!!</h2>
+                    </div>
+                </div>
+            )
+        })}
+      </div>
     </div>
-</div>
-  )
+  );
 }
