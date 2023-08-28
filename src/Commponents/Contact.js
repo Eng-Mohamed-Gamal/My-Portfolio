32633d2f6ffe8d5cbad78ad21b237f65a6811122
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-28 20:30:57 +0300

    M

--- src/Commponents/Contact.js
+++ src/Commponents/Contact.js
@@ -22,13 +22,13 @@ export default function Contact() {
             </a>
           </div>
         </div>
-        <form action="">
+        <form  action="https://formspree.io/f/xbjvoeen" method="POST">
           <div className="inputs">
-            <input type="text" placeholder="Name"/>
-            <input type="email" placeholder="Email"/>
-            <input type="text" placeholder="Subject" />
+            <input type="text" placeholder="Name" name="Name" required/>
+            <input type="email" placeholder="Email" name="Email" required />
+            <input type="text" placeholder="Subject"  name="Subject" required/>
           </div>
-          <textarea  cols="30" rows="10" className="area" placeholder="message"></textarea>
+          <textarea  cols="30" rows="10" className="area" name="Message" required placeholder="message"></textarea>
           <button type="submit">
             send message <div className="send"><i class="fa-regular fa-paper-plane"></i></div>
           </button>
