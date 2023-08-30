92f91da56c999d4d210b7c7e25bb7767b65c0ab5
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-30 03:59:45 +0300

    M

--- src/Commponents/Home.js
+++ src/Commponents/Home.js
@@ -1,54 +1,52 @@
-import React, { useEffect, useRef } from 'react'
-import { useTypewriter } from 'react-simple-typewriter'
+import React, { useEffect, useRef } from "react";
+import { useTypewriter } from "react-simple-typewriter";
 
-
-let string = "fsdfndjfbdjfkkjhk0123456789"
-let arr = []
-setInterval(()=>{
-        let random = string.split("")[Math.floor(Math.random() * string.split("").length  )]
-        let choose = random ;
-        arr.push(choose);
-        setTimeout(()=>{
-            arr.pop(choose)
-        },2000)
-    },20)
-
+let string = "fsdfndjfbdjfkkjhk0123456789";
+let arr = [];
+setInterval(() => {
+  let random =
+    string.split("")[Math.floor(Math.random() * string.split("").length)];
+  let choose = random;
+  arr.push(choose);
+  setTimeout(() => {
+    arr.pop(choose);
+  }, 3000);
+}, 20);
 
 export default function Home() {
-    const myRef = useRef()
-useEffect(()=>{
-        let ele = myRef.current
-        let left = Math.floor(Math.random() * 250)
-         let size =Math.floor( Math.random() * 60)
-         if(ele){
-             ele.style.fontSize = size + "px" ;
-             ele.style.left = left + "px" ;
-            }
-})
+  const myRef = useRef();
+  useEffect(() => {
+    let ele = myRef.current;
+    let left = Math.floor(Math.random() * 250);
+    let size = Math.floor(Math.random() * 60);
+    if (ele) {
+      ele.style.fontSize = size + "px";
+      ele.style.left = left + "px";
+    }
+  });
 
-const [text] = useTypewriter({
-        words : ["front-end-developer"],
-        loop : {}
-    })
+  const [text] = useTypewriter({
+    words: ["front-end-developer"],
+    loop: {},
+  });
 
   return (
-<div className="home" id='Home'>
-    <div className="box">
-        <div className="cloud" >
-            {arr.map((str , index)=>{
-                return (
-                    <div className="char" key={index} ref={myRef} >
-                        {str}
-                    </div>
-                )
-            })}
+    <div className="home" id="Home">
+      <div className="box">
+        <div className="cloud">
+          {arr.map((str, index) => {
+            return (
+              <div className="char" key={index} ref={myRef}>
+                {str}
+              </div>
+            );
+          })}
         </div>
-        <img src="assets/Me.jpg" alt="" />
-    </div>
-    <div className="con">
+      </div>
+      <div className="con">
         <h1>i’m mohamed gamal</h1>
-        <h2>  i’m  {text}</h2>
+        <h2> i’m {text}</h2>
+      </div>
     </div>
-</div>
-  )
+  );
 }
